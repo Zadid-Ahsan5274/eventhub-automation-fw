e8261c70bd4566da3e5db66740541caf41c856c4
Zadid-Ahsan5274 <zadid.ahsan5274@gmail.com>
2026-09-16 12:52:01 +0600

    Added and modified some files

---
+++ utils/constants.py
@@ -0,0 +1,15 @@
+"""
+Central place for routes, timeouts and other static constants used
+across the framework. Keeping routes here means page objects never
+hardcode raw query strings.
+"""
+
+class Routes:
+    HOME = "/"
+    LOGIN="/login"
+    REGISTER="/register"
+    EVENTS="/events"
+    BOOKINGS="/bookings"
+    MANAGE_EVENTS="/admin/events"
+
+class Timeouts:
